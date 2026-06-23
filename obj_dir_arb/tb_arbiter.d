c0f4aeacd8cc07bb3e91680881c852923ce190ba
Auresol <phudis27@gmail.com>
2026-06-24 03:12:12 +0700

    fix .gitignore obj folder

--- obj_dir_arb/tb_arbiter.d
+++
@@ -1,6 +0,0 @@
-tb_arbiter.o: ../sim/tb_arbiter.cpp Vahb_arbiter.h \
- /nix/store/pn95bkl3iqn4xr668bmvzdzls0fl2cv1-verilator-5.044/share/verilator/include/verilated.h \
- /nix/store/pn95bkl3iqn4xr668bmvzdzls0fl2cv1-verilator-5.044/share/verilator/include/verilated_config.h \
- /nix/store/pn95bkl3iqn4xr668bmvzdzls0fl2cv1-verilator-5.044/share/verilator/include/verilatedos.h \
- /nix/store/pn95bkl3iqn4xr668bmvzdzls0fl2cv1-verilator-5.044/share/verilator/include/verilated_types.h \
- /nix/store/pn95bkl3iqn4xr668bmvzdzls0fl2cv1-verilator-5.044/share/verilator/include/verilated_funcs.h
--- obj_dir_dec/tb_decoder.d
+++
@@ -1,6 +0,0 @@
-tb_decoder.o: ../sim/tb_decoder.cpp Vahb_decoder.h \
- /nix/store/pn95bkl3iqn4xr668bmvzdzls0fl2cv1-verilator-5.044/share/verilator/include/verilated.h \
- /nix/store/pn95bkl3iqn4xr668bmvzdzls0fl2cv1-verilator-5.044/share/verilator/include/verilated_config.h \
- /nix/store/pn95bkl3iqn4xr668bmvzdzls0fl2cv1-verilator-5.044/share/verilator/include/verilatedos.h \
- /nix/store/pn95bkl3iqn4xr668bmvzdzls0fl2cv1-verilator-5.044/share/verilator/include/verilated_types.h \
- /nix/store/pn95bkl3iqn4xr668bmvzdzls0fl2cv1-verilator-5.044/share/verilator/include/verilated_funcs.h
